956b39b948fa4a787b829f943df72b8d95626ea2
kevin <notkelbie@outlook.com>
2019-02-21 18:27:33 +0000

    handle genesis transaction edge case

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -3,26 +3,57 @@ var async = require("async");
 
 module.exports = {
   getTransaction: async (txid) => {
-    var options = {
-      url: "http://user:pass@127.0.0.1:18332",
-      method: "POST",
-      body: JSON.stringify({
-        jsonrpc: "1.0",
-        id: "curltest",
-        method: "getrawtransaction",
-        params: [
-          txid,
-          true
-        ]
-      })
-    };
+    // If txid corresponds to the genesis transaction
+    if (txid == "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b") {
 
-    var data = await request(options);
-    data = JSON.parse(data).result;
+      var hash = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
 
-    data.vins = data.vin;
-    data.vouts = data.vout;
+      var options = {
+        url: "http://user:pass@127.0.0.1:18332",
+        method: "POST",
+        body: JSON.stringify({
+          jsonrpc: "1.0",
+          id: "curltest",
+          method: "getblock",
+          params: [
+            hash,
+            2
+          ]
+        })
+      };
+  
+      var data = await request(options);
+      data = JSON.parse(data).result.tx[0];
 
-    return data;
+      data.vins = data.vin;
+      data.vouts = data.vout;
+
+      return data;
+
+    } else {
+
+      var options = {
+        url: "http://user:pass@127.0.0.1:18332",
+        method: "POST",
+        body: JSON.stringify({
+          jsonrpc: "1.0",
+          id: "curltest",
+          method: "getrawtransaction",
+          params: [
+            txid,
+            true
+          ]
+        })
+      };
+  
+      var data = await request(options);
+      data = JSON.parse(data).result;
+  
+      data.vins = data.vin;
+      data.vouts = data.vout;
+  
+      return data;
+    }
+
   }
 };
